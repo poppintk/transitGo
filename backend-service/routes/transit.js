4d31dfc8f82a2c161822b5a07756a9fc32b38073
Ryan YOU <ryou@broadviewsoftware.com>
2019-06-28 16:32:33 -0400

    code clean up

--- backend-service/routes/transit.js
+++ backend-service/routes/transit.js
@@ -3,10 +3,13 @@ const gtfs = require('gtfs');
 
 const router = express.Router();
 
+const capitalize = apiEndPoint =>
+  apiEndPoint.charAt(0).toUpperCase() + apiEndPoint.slice(1);
+
 router.post('/:apiEndPoint', async (req, res) => {
   const { apiEndPoint } = req.params;
   if (!apiEndPoint) return res.status(404).send('No Api end point.');
-  const cap = apiEndPoint.charAt(0).toUpperCase() + apiEndPoint.slice(1);
+  const cap = capitalize(apiEndPoint);
   const method = `get${cap}`;
   const body = req.body;
   if (!body) return res.status(404).send('Body is empty.');
