699f14c956ac8abccc613e773df333e8045db652
Ryan YOU <ryou@broadviewsoftware.com>
2019-06-28 11:08:18 -0400

    add gtfs data to node server

--- node/index.js
+++ node/index.js
@@ -2,12 +2,20 @@ const winston = require('winston');
 const express = require('express');
 
 const app = express();
+const gtfs = require('gtfs');
 
 require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
 require('./startup/validation')();
+
+// Get a specific agency by `agency_name`
+gtfs
+  .getAgencies({
+    agency_name: 'GO Transit'
+  })
+  .then(agencies => console.log(agencies));
 
 const port = process.env.PORT || 9000;
 const server = app.listen(port, () =>
